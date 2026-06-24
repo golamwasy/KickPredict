ba9e2e0a1841851170cbadf2f40912a58af69cd0
Golam Wasy Arnob <golamwasy@gmail.com>
2026-06-24 17:56:57 +0300

    fixed scotland flag image

--- frontend/src/app/utils/flags.ts
+++ frontend/src/app/utils/flags.ts
@@ -1,33 +1,33 @@
 // Map 3-letter team codes to emoji flags
 const CODE_MAP = {
   // Americas
-  USA:'US', CAN:'CA', MEX:'MX', BRA:'BR', ARG:'AR',
-  URU:'UY', COL:'CO', ECU:'EC', CHI:'CL', PER:'PE',
-  PAR:'PY', HON:'HN', CRC:'CR', PAN:'PA', JAM:'JM',
-  HAI:'HT', CUW:'CW',
+  USA: 'US', CAN: 'CA', MEX: 'MX', BRA: 'BR', ARG: 'AR',
+  URU: 'UY', COL: 'CO', ECU: 'EC', CHI: 'CL', PER: 'PE',
+  PAR: 'PY', HON: 'HN', CRC: 'CR', PAN: 'PA', JAM: 'JM',
+  HAI: 'HT', CUW: 'CW',
 
   // Europe
-  FRA:'FR', GER:'DE', ENG:'GB', ESP:'ES', POR:'PT',
-  ITA:'IT', NED:'NL', BEL:'BE', CRO:'HR', SRB:'RS',
-  SUI:'CH', DEN:'DK', POL:'PL', AUT:'AT', HUN:'HU',
-  CZE:'CZ', SVK:'SK', GRE:'GR', TUR:'TR', UKR:'UA',
-  SWE:'SE', NOR:'NO', FIN:'FI', ROM:'RO', SLO:'SI',
-  BIH:'BA', ISL:'IS', IRL:'IE', SCO:'GB', WAL:'GB',
+  FRA: 'FR', GER: 'DE', ENG: 'GB', ESP: 'ES', POR: 'PT',
+  ITA: 'IT', NED: 'NL', BEL: 'BE', CRO: 'HR', SRB: 'RS',
+  SUI: 'CH', DEN: 'DK', POL: 'PL', AUT: 'AT', HUN: 'HU',
+  CZE: 'CZ', SVK: 'SK', GRE: 'GR', TUR: 'TR', UKR: 'UA',
+  SWE: 'SE', NOR: 'NO', FIN: 'FI', ROM: 'RO', SLO: 'SI',
+  BIH: 'BA', ISL: 'IS', IRL: 'IE', SCO: 'GB-SCT', WAL: 'GB',
 
   // Africa
-  ALG:'DZ', MAR:'MA', SEN:'SN', NGA:'NG', GHA:'GH',
-  CMR:'CM', TUN:'TN', EGY:'EG', CIV:'CI', MLI:'ML',
-  ANG:'AO', ZAM:'ZM', ZIM:'ZW', KEN:'KE', GUI:'GN',
-  COD:'CD', CGO:'CG', CPV:'CV', RSA:'ZA',
+  ALG: 'DZ', MAR: 'MA', SEN: 'SN', NGA: 'NG', GHA: 'GH',
+  CMR: 'CM', TUN: 'TN', EGY: 'EG', CIV: 'CI', MLI: 'ML',
+  ANG: 'AO', ZAM: 'ZM', ZIM: 'ZW', KEN: 'KE', GUI: 'GN',
+  COD: 'CD', CGO: 'CG', CPV: 'CV', RSA: 'ZA',
 
   // Asia
-  JPN:'JP', KOR:'KR', AUS:'AU', IRN:'IR', SAU:'SA',
-  QAT:'QA', JOR:'JO', UAE:'AE', IRQ:'IQ', CHN:'CN',
-  KSA:'SA', OMA:'OM', UZB:'UZ', VIE:'VN', THA:'TH',
-  IND:'IN',
+  JPN: 'JP', KOR: 'KR', AUS: 'AU', IRN: 'IR', SAU: 'SA',
+  QAT: 'QA', JOR: 'JO', UAE: 'AE', IRQ: 'IQ', CHN: 'CN',
+  KSA: 'SA', OMA: 'OM', UZB: 'UZ', VIE: 'VN', THA: 'TH',
+  IND: 'IN',
 
   // Oceania
-  NZL:'NZ', FIJ:'FJ',
+  NZL: 'NZ', FIJ: 'FJ',
 };
 
 export function getFlag(code: string): string {
